6e2f5a2d90d5d637f2332233baa1cddab47c5149
neil.chen <545430831@qq.com>
2018-04-28 10:18:08 +0800

    005统一json返回 未初始化的bean

--- module_lib/src/main/java/com/neil/module_lib/util/util/GsonUtil.java
+++ module_lib/src/main/java/com/neil/module_lib/util/util/GsonUtil.java
@@ -1,29 +1,36 @@
 package com.neil.module_lib.util.util;
 
+import android.text.TextUtils;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 /**
  * Created by chen on 2018/4/17.
- *
+ * <p>
  * bean:json
  */
 public class GsonUtil {
 
     /**
      * 解析Json
-     * @return
+     *
+     * @return 不会返回null，只会返回未赋值的bean
      */
-    public static<T> T fromJson(String json,Class<T> clz) {
+    public static <T> T fromJson(String json, Class<T> clz) {
+        if (TextUtils.isEmpty(json)) {    //确保以json，生成bean
+            json = "{}";
+        }
         return getGson().fromJson(json, clz);
     }
 
 
     /**
      * 转换json去实体
+     *
      * @return
      */
-    public static<T> String toJson(T t) {
+    public static <T> String toJson(T t) {
         return getGson().toJson(t);
     }
 
